bed304e525cf7d49b6338a60c5104d0d65e3dba7
Tamara Pfyffer <e282265@sbl.ch>
2023-10-30 11:59:10 +0100

    Aufg. 1&2 fertig

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
 export function aufgabe01(args) {
   const input = args
   const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+   if (currentElement === "e") {
+    result.push("")
+   } else if (currentElement === "E" ) {
+     result.push("")
+   }
+   else {
+    result.push(currentElement)
+  }
+}
+
+  return result.join("")
+}
+
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+
+    // Wandle das aktuelle Element in einen Grossbuchstaben um
+    const capitalizedElement = currentElement.toUpperCase()
+    result.push(capitalizedElement)
   }
+  return result.join("")
+ }
+
+
+ export function aufgabe03(args) {
+  const input = args
+  const result = []
+
+  return result.join("")
+ }
+
+
+
+
+
+ export function aufgabe04(args) {
+  const input = args
+  const result = []
+
   return result.join("")
 }
